f6fef35eb3d61322ef53cbd1f34d9bbef91f521d
Emanuel Buholzer <emanuel0xb@gmail.com>
2023-06-23 00:56:27 +0200

    Make scripts executable, fix clion script and add omcp_blender build image script

---
+++ scripts/build_image.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+podman build --layers=false --omit-history=true .
--- scripts/clion.sh
+++ scripts/clion.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 colcon build --symlink-install --continue-on-error --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+. install/local_setup.bash
 clion build/compile_commands.json
